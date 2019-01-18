4a27ef01875d30b47ee55524b0a9ebfb6fbba476
Abrehamgit <gezahegnabereham@gmail.com>
2019-01-18 13:37:22 -0800

    card name made smaller

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
-const Card = ({name,email,id}) =>{
-	return(
-		<div className='bg-light-green grow size pa3 br2 ma3 dib tc bw2 shadow-4'>
-		    <img src={`https://robohash.org/${id}? 200 x 200`} alt='robo'/>
-		    <div>
-		    <h2> {name} </h2>
-		    
-		    <p>  {email} </p>
-		    </div>
+const Card = ({ name, email, id }) => {
+	return (
+		<div className="bg-light-green grow size pa3 br2 ma3 dib tc bw2 shadow-4">
+			<img src={`https://robohash.org/${id}? 200 x 200`} alt="robo" />
+			<div>
+				<h3> {name} </h3>
+
+				<p> {email} </p>
+			</div>
 		</div>
-	)
-}
-
+	);
+};
 
 export default Card;
